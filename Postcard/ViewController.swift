dad5de8014514d92bf5cf4fbc93dc6dc157ecf78
Alvin Lam <Alvin@192-168-1-13.tpgi.com.au>
2019-03-17 18:30:44 +1100

    Initial Commit

---
+++ Postcard/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Postcard
+//
+//  Created by Alvin Lam on 17/03/2019.
+//  Copyright © 2019 Alvin Lam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
